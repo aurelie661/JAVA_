ac488d380b4ed8649722fa66fc6f922aff295f46
aurelie661 <boutaurel59@gmail.com>
2023-07-19 11:56:47 +0200

    exo done

--- firstProject/src/exercices/PérimètreAndAire.java
+++ firstProject/src/exercices/PérimètreAndAire.java
@@ -1,12 +1,22 @@
 package exercices;
 
+import java.util.Scanner;
+
 public class PérimètreAndAire {
     public static void main(String[] args) {
         /*
         *1. Saisir la longueur du côté du carré et la stocker dans une variable
         * 2. Calculer l'aire et le périmètre du carré
         * */
+        double  longueur, resultatPerimetre, resultatAire;
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Saisir la longueur d'un coté :");
+        longueur = scanner.nextDouble();
 
+        resultatPerimetre = 4 * longueur;
+        resultatAire = longueur * longueur;
+        System.out.println("Le périmètre du carré est de : " + resultatPerimetre);
+        System.out.println("L'aire du carré est de : " + resultatAire);
     }
 }
