776e0cb4abb2ec8697f1848082fed4e3adc98603
aurelie661 <boutaurel59@gmail.com>
2023-07-19 10:54:05 +0200

    Ajout de l'énnoncer

--- firstProject/src/exercices/MajeurOuMineurVersion1.java
+++ firstProject/src/exercices/MajeurOuMineurVersion1.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class MajeurOuMineurVersion1 {
     public static void main(String[] args) {
+        /*1. Créer une variable age et lui affecter une valeur
+        2. Vérifier si la personne est mineure ou majeure à l'aide desopérateurs logiques
+        3. Afficher le résultat4.
+         /!\ Ne pas utiliser de structure conditionnelle*/
+
         int age = 46;
 
         Scanner scanner = new Scanner(System.in);
