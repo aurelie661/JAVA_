903330e340999c3af6889f237f5386586c1dce6b
aurelie661 <boutaurel59@gmail.com>
2023-07-19 10:34:36 +0200

    Ajout de l'énnoncer

--- firstProject/src/exercices/PrenomNom.java
+++ firstProject/src/exercices/PrenomNom.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class PrenomNom {
     public static void main(String[] args) {
+        /*1. Créer une variable nom et une variable prenom
+        2. Afficher la phrase suivante "Bonjour {prenom} {NOM}."
+        3. Remplacer les valeurs entre chevrons par les variables crééesprécédemment*/
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Saisir un prénom :");
---
+++ firstProject/src/exercices/PérimètreAndAire.java
@@ -0,0 +1,12 @@
+package exercices;
+
+public class PérimètreAndAire {
+    public static void main(String[] args) {
+        /*
+        *1. Saisir la longueur du côté du carré et la stocker dans une variable
+        * 2. Calculer l'aire et le périmètre du carré
+        * */
+
+
+    }
+}
