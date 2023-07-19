aee4b9f5e49a92d8fcc43c211f5678964d9a4935
aurelie661 <boutaurel59@gmail.com>
2023-07-19 14:34:01 +0200

    Ex Pair ou Impair fini

--- firstProject/src/exercices/PairOuImpair.java
+++ firstProject/src/exercices/PairOuImpair.java
@@ -4,10 +4,20 @@ import java.util.Scanner;
 
 public class PairOuImpair {
     public static void main(String[] args) {
+        /*1. Créer une variable nombre de type entier
+        2. Affecter une valeur à la variable nombre
+        3. Créer une condition qui permet d'afficher si le nombre est pair ou impair
+        Utiliser l'opérateur mathématique permettant de retourner le reste d'unedivision euclidienne*/
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Veuillez entrer un nombre :");
         int number = scanner.nextInt();
 
+        if (number % 2 == 0){
+            System.out.println("Le nombre saisie est pair");
+        }else{
+            System.out.println("Le nombre saisie est impair");
+        }
 
     }
 }
