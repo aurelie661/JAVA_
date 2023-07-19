a144a123f629a9063e12a13b1e39faa31104921c
aurelie661 <boutaurel59@gmail.com>
2023-07-19 10:25:03 +0200

    Avancement de la mise en forme

--- firstProject/src/exercices/MiseEnFormeString.java
+++ firstProject/src/exercices/MiseEnFormeString.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 
 public class MiseEnFormeString {
     public static void main(String[] args) {
+
+        /*1. Saisir une chaine et la stocker dans une variable2.
+        Effectuer les traitements suivants :
+        1. Afficher la chaîne en minuscule
+        2. Afficher la chaîne en majuscule
+        3. Bonus1. Convertir la chaîne en tableau puis afficher les caractères séparer d'une virgule
+        2. Afficher la première lettre de chaque mot en majuscule*/
+
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Saisir une chaine :");
         String chaine = scanner.nextLine();
         System.out.println(chaine);
-
+//------------------------------------------------------------------------
         String minus = chaine.toLowerCase();
         System.out.println("En minuscule : "+minus);
-
+//------------------------------------------------------------------------
         String majus = chaine.toUpperCase();
         System.out.println("En majuscule : "+majus);
-
+//------------------------------------------------------------------------
         char[] arr = new char[minus.length()];
         for (int i = 0; i < minus.length(); i++) {
             arr[i] = minus.charAt(i);
@@ -25,9 +33,18 @@ public class MiseEnFormeString {
             System.out.print(x+",");
         }
         System.out.println();
+//------------------------------------------------------------------------
+        String ch1 = Character.toString(minus.charAt(0)).toUpperCase();
 
+        System.out.println(minus.replaceFirst(ch1, String.valueOf(minus.indexOf(minus))));
+        /*String[] arrMinus = new String[minus.length()];
+        for (int i = 0; i < minus.length(); i++) {
+            if(arrMinus[i] == arrMinus[0]){
+                arrMinus[i] = ch1;
+            }
+            arrMinus[i] = arrMinus[i];
+        }*/
         System.out.print("Premiere lettre en majuscule : ");
-        String maj1 = minus;
-        System.out.println(maj1);
+        System.out.println();
     }
 }
