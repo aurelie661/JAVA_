5a49635ef1d7420ccf290759a5a5baded65663eb
aurelie661 <boutaurel59@gmail.com>
2023-07-19 10:54:42 +0200

    Ajout de l'énnoncer

--- firstProject/src/exercices/VolumeCercle.java
+++ firstProject/src/exercices/VolumeCercle.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class VolumeCercle {
     public static void main(String[] args) {
+        /*1. Créer un programme qui permet de calculer le volume d'un cône
+        2. La formule est la suivante : 1/3 × π × r² × h*/
+
         double  volume;
         Scanner scanner = new Scanner(System.in);
 
