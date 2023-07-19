c550d52c3aa36ebffb90d816f3f30c9fc0fe3fe7
aurelie661 <boutaurel59@gmail.com>
2023-07-19 13:35:56 +0200

    Ajout de l'énnoncer

--- firstProject/src/exercices/MajeurOuMineur.java
+++ firstProject/src/exercices/MajeurOuMineur.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class MajeurOuMineur {
     public static void main(String[] args) {
+    /*1. Créer une variable age
+    2. Affecter une valeur à la variable age
+    3. Créer une condition qui permet d'afficher si la personne estmajeure ou mineure*/
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Veuillez entrer votre age :");
