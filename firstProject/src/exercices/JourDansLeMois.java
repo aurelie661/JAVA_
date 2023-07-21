1f54fb797c2045533fa144c73a134e4243fe1049
aurelie661 <boutaurel59@gmail.com>
2023-07-21 16:32:43 +0200

    Exo jour dans le mois fini

---
+++ firstProject/src/exercices/JourDansLeMois.java
@@ -0,0 +1,35 @@
+package exercices;
+
+import java.util.Scanner;
+
+public class JourDansLeMois {
+    public static void main(String[] args) {
+        int day;
+
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println("Saisir un numéro de mois :");
+            day = scanner.nextInt();
+            if(day < 1 || day > 12){
+                System.out.println("Attention les mois vont de 1 à 12!");
+            }else {
+                switch (day){
+                    case 1,3,5,7,8,10,12 :
+                        System.out.println("31 jours");
+                        break;
+                    case 4,6,9,11 :
+                        System.out.println("30 jours");
+                        break;
+                    case 2 :
+                        System.out.println("28 jours les années standards et 29 les nnées bissextiles");
+                        break;
+                    default:
+                        System.out.println("pffff");
+                }
+            }
+
+        }while(day < 1 || day > 7);
+        System.out.println("ok c'est pafait , merci");
+    }
+}
