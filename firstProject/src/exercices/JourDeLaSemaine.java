a9e79064895f6dfebcef2ed5055cc0aa50d9d781
aurelie661 <boutaurel59@gmail.com>
2023-07-21 16:15:25 +0200

    Exo jour de la semaine fini

--- firstProject/src/exercices/JourDeLaSemaine.java
+++ firstProject/src/exercices/JourDeLaSemaine.java
@@ -12,9 +12,42 @@ public class JourDeLaSemaine {
         int day;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Saisir un jour de la semaine :");
-        day = scanner.nextInt();
 
+        do {
+            System.out.println("Saisir un jour de la semaine :");
+            day = scanner.nextInt();
+            if(day < 1 || day > 7){
+                System.out.println("Attention les jours de la semaine vont de 1 à 7!");
+            }else {
+                switch (day){
+                    case 1 :
+                        System.out.println("Lundi");
+                        break;
+                    case 2 :
+                        System.out.println("Mardi");
+                        break;
+                    case 3 :
+                        System.out.println("Mercredi");
+                        break;
+                    case 4 :
+                        System.out.println("Jeudi");
+                        break;
+                    case 5 :
+                        System.out.println("Vendredi");
+                        break;
+                    case 6 :
+                        System.out.println("Samedi");
+                        break;
+                    case 7 :
+                        System.out.println("Dimanche");
+                        break;
+                    default:
+                        System.out.println("pffff");
 
+                }
+            }
+
+        }while(day < 1 || day > 7);
+        System.out.println("ok c'est pafait , merci");
     }
 }
