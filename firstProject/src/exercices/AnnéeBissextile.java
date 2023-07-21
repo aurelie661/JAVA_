9c7b452e5378efb1415839017fba33e3b9ae47ae
aurelie661 <boutaurel59@gmail.com>
2023-07-21 16:41:42 +0200

    Exo année bissextile fini

---
+++ firstProject/src/exercices/AnnéeBissextile.java
@@ -0,0 +1,31 @@
+package exercices;
+
+import java.util.Scanner;
+
+public class AnnéeBissextile {
+    static Boolean bissextile(int annee) {
+        if(annee%4 == 0 && annee%100 != 0) {
+            return true;
+        }
+        else if(annee%400 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        int annee;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir une année :");
+        annee = scanner.nextInt();
+        if(bissextile(annee)){
+            System.out.println(bissextile(annee));
+            System.out.println(annee +" est une année bissextile");
+        }else{
+            System.out.println(bissextile(annee));
+            System.out.println(annee +" n'est pas une année bissextile");
+        }
+    }
+}
