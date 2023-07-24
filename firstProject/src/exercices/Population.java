344944d6a60d73955f0a9e9766411a219e8d9eda
aurelie661 <boutaurel59@gmail.com>
2023-07-24 14:20:38 +0200

    Exo Population

---
+++ firstProject/src/exercices/Population.java
@@ -0,0 +1,13 @@
+package exercices;
+
+public class Population {
+    public static void main(String[] args) {
+        /*1.L'accroissement de la population de tourcoing est de 0.89%
+        * 2.En 2015 la ville comptait 96 809 habitants
+        * 3. Combien d'années faut-il pour atteindre 120 000 habitants ?
+        * 4. Combien d'habitants y aur-t-il cette année-la
+        * 5. Ecrire un programme permettant de résoudre ce problême*/
+
+
+    }
+}
