fcb2b6d7d1ce4aaaf9a5a56634dc5a0da2586e3f
aurelie661 <boutaurel59@gmail.com>
2023-07-24 10:57:13 +0200

    Exo Lettre, nombre ou caratère spécial fini

---
+++ firstProject/src/exercices/LettreNombreCaractereSpecial.java
@@ -0,0 +1,28 @@
+package exercices;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LettreNombreCaractereSpecial {
+    public static void main(String[] args) {
+        /*1. Créer une variable caractere
+        * 2. Affecter une valeur à la variable caractere
+        * 3. Afficher un message en fonction du type du caractere (lettre,nombre ou caractere special*/
+
+        char character;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir un caractere");
+        character = scanner.next().charAt(0);
+
+
+        if ((character >= 65 && character <= 90) || (character >= 97 && character <= 122)){
+            System.out.println(character+ " : est une lettre de l'alphabet");
+        }else if (character >= 48 && character <= 57) {
+            System.out.println(character+ " : est un caractère numérique");
+        }else {
+            System.out.println(character+ " : est un caractère spécial ");
+        }
+    }
+}
