e41c5877cf3ca0409f2dfa8c537232386bef092d
aurelie661 <boutaurel59@gmail.com>
2023-07-24 13:51:48 +0200

    Exo De 1 à 10 fini

---
+++ firstProject/src/exercices/De1à10.java
@@ -0,0 +1,12 @@
+package exercices;
+
+public class De1à10 {
+    public static void main(String[] args) {
+        /*1. Ecrire un programme qui affiche les nombres de 1 à 10 à l'aide d'une boucle
+        * 2.Afficher la valeur à chaque itération*/
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+    }
+}
