b4b27da767a6312035ada815bcd0e950563e54f8
aurelie661 <boutaurel59@gmail.com>
2023-07-24 21:55:49 +0200

    Exo Calcul de la population

--- firstProject/src/exercices/Population.java
+++ firstProject/src/exercices/Population.java
@@ -5,9 +5,17 @@ public class Population {
         /*1.L'accroissement de la population de tourcoing est de 0.89%
         * 2.En 2015 la ville comptait 96 809 habitants
         * 3. Combien d'années faut-il pour atteindre 120 000 habitants ?
-        * 4. Combien d'habitants y aur-t-il cette année-la
+        * 4. Combien d'habitants y aura-t-il cette année-la
         * 5. Ecrire un programme permettant de résoudre ce problême*/
 
+        int annees = 2015;
+        double habitants = 96_809;
 
+        while (habitants < 120_000) {
+            habitants *= 1.0089;
+            annees++;
+        }
+
+        System.out.printf("La population de Tourcoing dépassera les 120 000 habitants en %s pour %s habitants", annees, (int) Math.ceil(habitants));
     }
 }
