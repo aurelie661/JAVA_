fdfb0132e6657007bc015449927c9ae9bd61c94e
aurelie661 <boutaurel59@gmail.com>
2023-07-24 14:08:27 +0200

    Exo Tables de multiplications fini

---
+++ firstProject/src/exercices/TableMultication.java
@@ -0,0 +1,15 @@
+package exercices;
+
+public class TableMultication {
+    public static void main(String[] args) {
+        /*1. Créer un programme permettant d'afficher les tables de multiplication de 1 à 10*/
+
+        for (int i = 1; i < 11; i++) {
+            System.out.printf("Table de %d\n",i);
+            for (int j = 1; j < 11; j++) {
+                System.out.printf("\t%d X %d = %d\n",i,j,i*j);
+
+            }
+        }
+    }
+}
